c2240c697bceb0d1473348f165ce9761b1a64cae
satryaputra <mohammadekasatryaputra@gmail.com>
2025-07-28 18:50:38 +0700

    feat: add adaptor cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://satrya.dev",
   integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: cloudflare(),
 });
